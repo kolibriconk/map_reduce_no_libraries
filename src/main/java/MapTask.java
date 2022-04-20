02f82974438fbc984370a2d49bf9d8573a31ada0
1565479 - Jose A. Ramos Andrades <joseantonio.ramosa@autonoma.cat>
2022-04-20 14:04:29 +0200

    Impreved calculation and checked how to calculate frequency.
    Yet to parallelize shuffle phase and try to optimize code.

--- src/main/java/MapTask.java
+++ src/main/java/MapTask.java
@@ -2,13 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MapTask implements Runnable {
-    private final String line;
-    private List<KeyValuePair<Character, Integer>> result;
-    private final int count;
+    private String line;
+    private final List<KeyValuePair<Character, Integer>> result;
+    private int count;
 
     public MapTask(String line) {
-        line = line.replaceAll("[^\\p{L}]", "");
-        line = line.trim();
         this.line = line;
         this.result = new ArrayList<>();
         this.count = line.length();
@@ -16,9 +14,27 @@ public class MapTask implements Runnable {
 
     @Override
     public void run() {
-        for (char letter : this.line.toCharArray()) {
-            result.add(new KeyValuePair<>(letter, 1));
+        this.count = this.line.split(" ").length;
+        this.line = this.line.toLowerCase();
+        this.line = line.replaceAll("[^\\w\\s\\pL]", "");
+
+        List<KeyValuePair<Character, Integer>> auxList = new ArrayList<>();
+        for (String word : this.line.split(" ")) {
+            auxList.clear();
+            for (char letter : word.toCharArray()) {
+                if(!checkIfKeyValueHasKey(letter, auxList)) {
+                    auxList.add(new KeyValuePair<>(letter, 1));
+                }
+            }
+            result.addAll(auxList);
         }
+    }
+
+    private boolean checkIfKeyValueHasKey(char letter, List<KeyValuePair<Character, Integer>> auxList) {
+        for (KeyValuePair<Character, Integer> pair : auxList) {
+            if (pair.getKey() == letter) return true;
+        }
+        return false;
     }
 
     public List<KeyValuePair<Character, Integer>> getResult() {
