4cc1ad0a06837bc119b33696d2ae775f5e456fe5
estufamagica <57752904+estufamagica@users.noreply.github.com>
2022-04-22 11:37:12 +0200

    Sequential mode implemented

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,6 +10,81 @@ import java.util.concurrent.TimeUnit;
 
 public class Client {
     public static void main(String[] args) {
+        if (args[args.length-1].equals("seq")) sequential(Arrays.copyOf(args, args.length-1));
+        else if(args[args.length-1].equals("par")) parallel(Arrays.copyOf(args, args.length-1));
+        else System.out.println("Cannot find execution mode");
+    }
+
+
+    public static void sequential(String[] args){
+        for (String arg:args){
+            BufferedReader br= null;
+            try{
+                int totalletters = 0;
+                System.out.println("Reading file " + arg);
+                br = new BufferedReader(new FileReader(arg));
+                String line;
+                List<KeyValuePair<Character, Float>> Mapresult = new ArrayList<>();
+                while ((line = br.readLine())!= null){ //fase map
+                    totalletters += line.split(" ").length;
+                    line=line.toLowerCase();
+                    line=line.replaceAll("[^\\w\\s\\pL]", "");
+                    List<KeyValuePair<Character, Float>> auxList = new ArrayList<>();
+                    for (String word: line.split(" ")){
+                        auxList.clear();
+                        for (char letter : word.toCharArray()){
+                            boolean trobat = false;
+                            for (KeyValuePair<Character, Float> kvp:auxList){
+                                if (kvp.getKey()== letter) {
+                                    trobat=true;
+                                    break;
+                                }
+                            }
+                            if (!trobat){
+                                auxList.add(new KeyValuePair<>(letter, 1.f));
+                            }
+                        }
+                        Mapresult.addAll(auxList);
+                    }
+                }
+                //fase shuffle
+                List<KeyValuePair<Character, Float>> Result = new ArrayList<>();
+                List<Character> alreadyAdded = new ArrayList<>();
+                for (KeyValuePair<Character, Float> kvp:Mapresult){
+                    if (!alreadyAdded.contains(kvp.getKey())){
+                        Result.add(kvp);
+                        alreadyAdded.add(kvp.getKey());
+                    }
+                    else
+                    {
+                        Result.get(alreadyAdded.indexOf(kvp.getKey())).setValue(Result.get(alreadyAdded.indexOf(kvp.getKey())).getValue()+1);
+                    }
+                }
+
+                //fase reduce
+                for (KeyValuePair<Character, Float> kvp:Result){
+                    kvp.setValue((kvp.getValue()/totalletters)*100);
+                    System.out.println(kvp.getKey() + " : " + kvp.getValue() + " %");
+                }
+
+                System.out.println("Finished reading file " + arg);
+
+            } catch(IOException e){
+                System.out.println("File not found, skipping");
+            } finally {
+                if (br != null){
+                    try{
+                        br.close();
+                    }catch(IOException e){
+                        System.out.println("Cannot close the buffered reader");
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static void parallel(String[] args){
         for (String arg : args) {
             BufferedReader br = null;
             try {
