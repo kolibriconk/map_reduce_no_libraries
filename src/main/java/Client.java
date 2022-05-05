7de2022ea901006334e8b2abc2f89323b965c12a
1565479 - Jose A. Ramos Andrades <joseantonio.ramosa@autonoma.cat>
2022-05-05 13:57:46 +0200

    Added final changes to code

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,3 @@
-import javax.swing.text.Utilities;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -22,14 +21,12 @@ public class Client {
 
         if (args.length != 0) {
             List<String> argList = Arrays.asList(args);
-            long startTime = System.currentTimeMillis();
-
             if (argList.contains("-p") || argList.contains("--parallel")) {
-                parallel(argList.subList(1, argList.size()), MAX_CORES, true);
+                parallel(argList.subList(1, argList.size()), MAX_CORES, 10000, true);
             }
 
             if (argList.contains("-s") || argList.contains("--sequential")) {
-                sequential(argList.subList(1, argList.size()), true);
+                sequential(argList.subList(1, argList.size()), 10000, true);
             }
 
             if (argList.contains("-b") || argList.contains("--benchmark")) {
@@ -44,68 +41,110 @@ public class Client {
             if (argList.contains("-c") || argList.contains("--clean")) {
                 cleanTemps();
             }
-
-            long finishTime = System.currentTimeMillis();
-            System.out.println("Total time taken : " + (finishTime - startTime) + " ms");
-
         } else {
             System.out.println("Usage: java Client [OPTION] [FILE1] [FILE2] ...");
         }
     }
 
     private static void benchmark(List<String> files) {
-        long startTime = System.currentTimeMillis();
-        sequential(files, false);
-        long finishTime = System.currentTimeMillis();
-        System.out.println("Sequential mode takes : " + (finishTime - startTime) + " ms");
+        long startTime;
+        long finishTime;
+        for (int i = 1000; i < 10000000; i = i * 2) {
+            startTime = System.currentTimeMillis();
+            sequential(files, i, false);
+            finishTime = System.currentTimeMillis();
+            System.out.printf("Sequential mode and %d line splitting takes : %d ms\n", i, (finishTime - startTime));
+        }
 
         for (int i = 1; i < MAX_CORES; i++) {
-            startTime = System.currentTimeMillis();
-            parallel(files, i, false);
-            finishTime = System.currentTimeMillis();
-            System.out.printf("Parallel mode with %d threads takes : %d ms\n", i, (finishTime - startTime));
+            for (int j = 1000; j < 10000000; j = j * 2) {
+                startTime = System.currentTimeMillis();
+                parallel(files, i, j, false);
+                finishTime = System.currentTimeMillis();
+                System.out.printf("Parallel mode with %d threads and %d lines splitting takes : %d ms\n", i, j, (finishTime - startTime));
+            }
         }
     }
 
-    public static void sequential(List<String> files, boolean usePrint) {
+    public static void sequential(List<String> files, int lineSplittingFactor, boolean usePrint) {
         for (String file : files) {
-//            BufferedReader br = null;
-//            try {
-//                int totalWords = 0;
-//                //Input of the program
-//                if (usePrint) System.out.printf("%s:\n", file);
-//                br = new BufferedReader(new FileReader(file));
-//                String line;
-//                List<KeyValuePair<Character, List<Integer>>> result = new ArrayList<>();
-//                while ((line = br.readLine()) != null) {
-//                    //Executing the map phase
-//                    MapTask mapTask = new MapTask(line);
-//                    KeyValuePair<Integer, List<KeyValuePair<Character, List<Integer>>>> resultCalled = mapTask.call();
-//                    result.addAll(resultCalled.getValue());
-//                    totalWords += resultCalled.getKey();
-//                    mapTask = null;
-//                }
-//
-//                List<KeyValuePair<Character, List<Integer>>> shuffledList = shuffle(result, new ArrayList<>());
-//                reduceSequential(shuffledList, totalWords, usePrint);
-//
-//            } catch (IOException e) {
-//                System.out.println("File not found, skipping");
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            } finally {
-//                if (br != null) {
-//                    try {
-//                        br.close();
-//                    } catch (IOException e) {
-//                        System.out.println("Cannot close the buffered reader");
-//                    }
-//                }
-//            }
+            cleanTemps();
+            BufferedReader br = null;
+            try {
+                int totalWords = 0;
+                //Input of the program
+                if (usePrint) System.out.printf("%s:\n", file);
+                long lines = Files.lines(Paths.get(file)).count();
+                long currentLine = 0;
+                int fileCounter = 0;
+                br = new BufferedReader(new FileReader(file));
+                String line;
+                StringBuilder sb = new StringBuilder();
+                List<KeyValuePair<Integer, List<KeyValuePair<Character, List<Integer>>>>> futures = new ArrayList<>();
+
+                while ((line = br.readLine()) != null) {
+                    sb.append(line)
+                            .append(" ");
+                    currentLine++;
+                    if (currentLine % lineSplittingFactor == 0 && currentLine != 0 || lines == currentLine) {
+                        //Executing the map phase
+                        MapTask mapTask = new MapTask(sb.toString());
+                        sb.setLength(0);
+                        futures.add(mapTask.call());
+                        //System.out.println("Queued " + currentLine + " from " + lines);
+                    }
+                    if (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() > MAX_MEMORY_THRESHOLD || (lines == currentLine)) {
+                        //System.out.println("Memory limit reached, waiting for tasks to finish");
+                        if (futures.size() > 0) {
+                            File outFile = new File("temp/temp_" + fileCounter + ".txt");
+                            FileOutputStream fos = new FileOutputStream(outFile, true);
+                            BufferedOutputStream bos = new BufferedOutputStream(fos);
+                            ObjectOutputStream oos = new ObjectOutputStream(bos);
+
+                            for (KeyValuePair<Integer, List<KeyValuePair<Character, List<Integer>>>> future : futures) {
+                                totalWords += future.getKey();
+                                oos.writeObject(future.getValue());
+                                future = null;
+                            }
+                            oos.flush();
+                            oos.close();
+                            bos.flush();
+                            bos.close();
+                            fos.flush();
+                            fos.close();
+                            fileCounter++;
+                            futures.clear();
+                        }
+                    }
+                }
+
+                if (futures.size() > 0) {
+                    for (KeyValuePair<Integer, List<KeyValuePair<Character, List<Integer>>>> future : futures) {
+                        totalWords += future.getKey();
+                    }
+                    futures.clear();
+                }
+
+                List<Character> alreadyAdded = shuffle(fileCounter);
+
+                reduceSequential(totalWords, alreadyAdded, usePrint);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        System.out.println("Cannot close the buffered reader");
+                    }
+                }
+            }
         }
     }
 
-    public static void parallel(List<String> files, int threadNumber, boolean usePrint) {
+    public static void parallel(List<String> files, int threadNumber, int lineSplittingFactor, boolean usePrint) {
         for (String file : files) {
             cleanTemps();
             BufferedReader br = null;
@@ -129,15 +168,15 @@ public class Client {
                     sb.append(line)
                             .append(" ");
                     currentLine++;
-                    if (currentLine % 10000 == 0 && currentLine != 0 || lines == currentLine) {
+                    if (currentLine % lineSplittingFactor == 0 && currentLine != 0 || lines == currentLine) {
                         //Executing the map phase
                         MapTask mapTask = new MapTask(sb.toString());
                         sb.setLength(0);
                         futures.add(es.submit(mapTask));
-                        System.out.println("Queued " + currentLine + " from " + lines);
+                        //System.out.println("Queued " + currentLine + " from " + lines);
                     }
                     if (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() > MAX_MEMORY_THRESHOLD || (lines == currentLine)) {
-                        System.out.println("Memory limit reached, waiting for tasks to finish");
+                        //System.out.println("Memory limit reached, waiting for tasks to finish");
                         es.shutdown();
                         if (es.awaitTermination(5, TimeUnit.MINUTES)) {
                             if (futures.size() > 0) {
@@ -196,7 +235,8 @@ public class Client {
         }
     }
 
-    static List<BufferedWriter> bufferedOutputStreams = new ArrayList<>();
+    static volatile List<BufferedWriter> bufferedOutputStreams = new ArrayList<>();
+    static volatile List<FileWriter> fileWriters = new ArrayList<>();
 
     private static List<Character> shuffle(int fileCounter) throws IOException, ClassNotFoundException {
         List<Character> alreadyAdded = new ArrayList<>();
@@ -204,7 +244,7 @@ public class Client {
 
         for (int i = 0; i < fileCounter; i++) {
             File inFile = new File("temp/temp_" + i + ".txt");
-            System.out.println("Processing file " + inFile.getName());
+            //System.out.println("Processing file " + inFile.getName());
             if (inFile.exists()) {
                 FileInputStream fos = new FileInputStream(inFile);
                 BufferedInputStream bos = new BufferedInputStream(fos);
@@ -218,17 +258,24 @@ public class Client {
                         }
                     }
                 }
+                oos.close();
+                bos.close();
+                fos.close();
             }
         }
 
-        System.out.println("Shuffle finished initiating stream closing");
+        //System.out.println("Shuffle finished initiating stream closing");
 
         for (BufferedWriter bufferedOutputStream : bufferedOutputStreams) {
             bufferedOutputStream.flush();
             bufferedOutputStream.close();
         }
 
-        System.out.println("Closed all streams");
+        for (FileWriter fileWriter : fileWriters) {
+            fileWriter.close();
+        }
+
+        //System.out.println("Closed all streams");
 
         return alreadyAdded;
     }
@@ -238,7 +285,9 @@ public class Client {
             alreadyAdded.add(pair.getKey());
             int index = alreadyAdded.indexOf(pair.getKey());
             File outFile = new File("temp/temp_shuffled_" + index + ".txt");
-            BufferedWriter bos = new BufferedWriter(new FileWriter(outFile, true));
+            FileWriter fw = new FileWriter(outFile, true);
+            fileWriters.add(fw);
+            BufferedWriter bos = new BufferedWriter(fw);
             bufferedOutputStreams.add(bos);
         }
         int index = alreadyAdded.indexOf(pair.getKey());
@@ -260,33 +309,32 @@ public class Client {
         if (es.awaitTermination(15, TimeUnit.MINUTES) && usePrint) {
             for (Future<KeyValuePair<Character, Float>> future : futures) {
                 KeyValuePair<Character, Float> kvp = future.get();
-                System.out.printf("%s : %.2f%s", kvp.getKey(), kvp.getValue(), "%\n");
+                if (usePrint) System.out.printf("%s : %.2f%s", kvp.getKey(), kvp.getValue(), "%\n");
             }
         }
     }
 
-//    public static void reduceSequential(List<KeyValuePair<Character, List<Integer>>> separatedKeyValuePairs, int totalLetters, boolean usePrint) throws InterruptedException {
-//        for (KeyValuePair<Character, List<Integer>> list : separatedKeyValuePairs) {
-//            ReduceTask reduceTask = new ReduceTask(list, totalLetters);
-//            reduceTask.run();
-//            KeyValuePair<Character, Float> kvp = reduceTask.getResult();
-//            if (usePrint) System.out.printf("%s : %.2f%s", kvp.getKey(), kvp.getValue(), "%\n");
-//        }
-//    }
+    public static void reduceSequential(int totalWords, List<Character> alreadyAdded, boolean usePrint) throws IOException, ClassNotFoundException, InterruptedException, ExecutionException {
+        for (int i = 0; i < alreadyAdded.size(); i++) {
+            String fileName = "temp/temp_shuffled_" + i + ".txt";
+            ReduceTask reduceTask = new ReduceTask(fileName, totalWords, alreadyAdded.get(i));
+            KeyValuePair<Character, Float> kvp = reduceTask.call();
+            if (usePrint) System.out.printf("%s : %.2f%s", kvp.getKey(), kvp.getValue(), "%\n");
+        }
+    }
 
-    private static boolean cleanTemps() {
+    private static void cleanTemps() {
         boolean success = false;
         try {
             File tempDir = new File("temp");
             File[] tempFiles = tempDir.listFiles();
             if (tempFiles != null) {
                 for (File tempFile : tempFiles) {
-                    while (tempFile.delete()) ;
+                    tempFile.delete();
                 }
             }
         } catch (Exception e) {
             System.out.println("Cannot delete the temp directory");
         }
-        return success;
     }
 }
--- src/main/java/ReduceTask.java
+++ src/main/java/ReduceTask.java
@@ -6,8 +6,12 @@ public record ReduceTask(String inputFilePath
         , long divisor, char key) implements Callable<KeyValuePair<Character, Float>> {
 
     @Override
-    public KeyValuePair<Character, Float> call() throws Exception {
-        float lines = Files.lines(Paths.get(inputFilePath)).count();
+    public KeyValuePair<Character, Float> call() {
+        float lines = 0;
+        try {
+            lines = Files.lines(Paths.get(inputFilePath)).count();
+        } catch (Exception ignored) {
+        }
 
         return new KeyValuePair<>(key, (lines / divisor) * 100);
     }
